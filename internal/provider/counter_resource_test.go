3150ecf07849d63294bd6becab02582654949535
Taneli Leppä <taneli@google.com>
2023-02-24 17:41:15 +0100

    Fixed tests.

--- internal/provider/counter_resource_test.go
+++ internal/provider/counter_resource_test.go
@@ -15,33 +15,33 @@ func TestAccPersistentCounterResource(t *testing.T) {
 			{
 				Config: testAccCounterResourceConfig(),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("persistent_counter.test", "initial_value", "0"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "last_value", "2"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "values.a", "0"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "values.b", "1"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "values.c", "2"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "initial_value", "0"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "last_value", "2"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "values.a", "0"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "values.b", "1"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "values.c", "2"),
 				),
 			},
 			{
 				Config: testAccCounterAddResourceConfig(),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("persistent_counter.test", "initial_value", "0"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "last_value", "3"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "values.a", "0"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "values.b", "1"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "values.c", "2"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "values.d", "3"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "initial_value", "0"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "last_value", "3"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "values.a", "0"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "values.b", "1"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "values.c", "2"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "values.d", "3"),
 				),
 			},
 			{
 				Config: testAccCounterUpdateResourceConfig(),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("persistent_counter.test", "initial_value", "0"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "last_value", "4"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "values.a", "0"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "values.b", "1"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "values.d", "3"),
-					resource.TestCheckResourceAttr("persistent_counter.test", "values.e", "4"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "initial_value", "0"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "last_value", "4"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "values.a", "0"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "values.b", "1"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "values.d", "3"),
+					resource.TestCheckResourceAttr("persistent_counter_values.test", "values.e", "4"),
 				),
 			},
 		},
@@ -50,8 +50,8 @@ func TestAccPersistentCounterResource(t *testing.T) {
 
 func testAccCounterResourceConfig() string {
 	return `
-resource "persistent_counter" "test" {
-  provider = persistent-counter
+resource "persistent_counter_values" "test" {
+  provider      = persistent-counter
   initial_value = 0
   keys          = ["a", "b", "c"]
 }
@@ -60,8 +60,8 @@ resource "persistent_counter" "test" {
 
 func testAccCounterAddResourceConfig() string {
 	return `
-resource "persistent_counter" "test" {
-  provider = persistent-counter
+resource "persistent_counter_values" "test" {
+  provider      = persistent-counter
   initial_value = 0
   keys          = ["a", "b", "c", "d"]
 }
@@ -70,8 +70,8 @@ resource "persistent_counter" "test" {
 
 func testAccCounterUpdateResourceConfig() string {
 	return `
-resource "persistent_counter" "test" {
-  provider = persistent-counter
+resource "persistent_counter_values" "test" {
+  provider      = persistent-counter
   initial_value = 0
   keys          = ["a", "b", "d", "e"]
 }
